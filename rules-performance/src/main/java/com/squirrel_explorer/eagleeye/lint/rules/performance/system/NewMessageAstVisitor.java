c8647460b29a01b556d7f4a7703f31c07e9e9f82
squirrel-explorer <xvider.zx@gmail.com>
2016-07-04 17:30:50 +0800

    更新NewMessageDetector规则，提升检测的准确性

--- rules-performance/src/main/java/com/squirrel_explorer/eagleeye/lint/rules/performance/system/NewMessageAstVisitor.java
+++ rules-performance/src/main/java/com/squirrel_explorer/eagleeye/lint/rules/performance/system/NewMessageAstVisitor.java
@@ -1,10 +1,10 @@
 package com.squirrel_explorer.eagleeye.lint.rules.performance.system;
 
+import com.android.tools.lint.client.api.JavaParser;
 import com.android.tools.lint.detector.api.JavaContext;
 import com.squirrel_explorer.eagleeye.types.base.BaseAstVisitor;
 
 import lombok.ast.ConstructorInvocation;
-import lombok.ast.TypeReference;
 
 /**
  * Created by squirrel-explorer on 16/02/22.
@@ -16,8 +16,14 @@ public class NewMessageAstVisitor extends BaseAstVisitor {
 
     @Override
     public boolean visitConstructorInvocation(ConstructorInvocation node) {
-        TypeReference typeRef = node.astTypeReference();
-        if (null != typeRef && "Message".equals(typeRef.getTypeName())) {
+        JavaParser.ResolvedNode resolvedNode = mContext.resolve(node);
+        if (!(resolvedNode instanceof JavaParser.ResolvedMethod)) {
+            return super.visitConstructorInvocation(node);
+        }
+        JavaParser.ResolvedMethod resolvedMethod = (JavaParser.ResolvedMethod)resolvedNode;
+
+        JavaParser.ResolvedClass typeClass = resolvedMethod.getContainingClass();
+        if (null != typeClass && typeClass.isSubclassOf("android.os.Message", false)) {
             mContext.report(
                     NewMessageDetector.ISSUE,
                     mContext.getLocation(node),
