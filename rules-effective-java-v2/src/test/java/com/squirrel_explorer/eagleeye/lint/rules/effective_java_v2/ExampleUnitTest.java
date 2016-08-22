0e32d7579f3530fc3b022f0e8e67f031c58cdc94
squirrel-explorer <xvider.zx@gmail.com>
2016-08-22 11:43:12 +0800

    准备Effective Java 2nd version的规则库

---
+++ rules-effective-java-v2/src/test/java/com/squirrel_explorer/eagleeye/lint/rules/effective_java_v2/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.squirrel_explorer.eagleeye.lint.rules.effective_java_v2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
